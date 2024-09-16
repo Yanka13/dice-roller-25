fad58360a14b511a98d8ffc89a57cf135f764def
yannis bouhamou <yannis.bouhamou@hotmail.fr>
2024-09-16 13:09:49 +0200

    Now API returns integer between 1 and 6

--- wsgi.py
+++ wsgi.py
@@ -1,11 +1,12 @@
 from flask import Flask, jsonify
+import random 
 
 app = Flask(__name__) 
 
 @app.route('/')
 def roll():
     
-    return jsonify({"roll": 0})
+    return jsonify({"roll": random.randint(1,6)})
 
 
 if __name__ == '__main__':
